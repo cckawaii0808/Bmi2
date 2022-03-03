7ecb16c1c43580312201ef90b97ac6a66c05fe2c
cc <cc_xie@gmail.com>
2022-03-03 17:10:15 +0800

    BMI Finish

--- app/src/main/java/com/example/bmi2/MainActivity.kt
+++ app/src/main/java/com/example/bmi2/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.bmi2
 
+import android.app.Instrumentation
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import com.example.bmi2.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
+    lateinit var binding: ActivityMainBinding //lateinit 晚點給
+    override fun onCreate(savedInstanceState: Bundle?) { //oncreate 一定會被執行
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+       //宣告取得外觀中的變數
+        binding=ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    fun bmibottom(view: View){  //方法後為按鈕onclick設定的名字
+        var weight=binding.edWeight.text.toString().toFloat()
+        //可變變數體重的值為binding裡的體重的資料轉文字再轉浮點數
+        var height=binding.edHeight.text.toString().toFloat()
+        val bmi =weight/(height*height)
+        Log.d("BMI為",bmi.toString())
+
     }
 }
